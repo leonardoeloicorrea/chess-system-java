4d8075fc045debda2fe6a2eaee3079cb8ab196c6
Leonardo Correa <leonardoeloi24@gmail.com>
2022-10-05 11:41:16 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -25,7 +25,6 @@ public class Position {
         this.column = column;
     }
 
-
     @Override
     public String toString() {
         return row + ", " + column;
---
+++ src/chess/ChessExceptions.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class ChessExceptions extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L;
+
+    public ChessExceptions(String msg) {
+        super(msg);
+    }
+
+
+}
